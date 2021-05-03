aa8bdced1850bbcf1bbaed7ff8b9da2f3ee73c1d
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-05-03 21:16:22 +0200

    Esercizio completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,26 +2,45 @@
 
 
 function s1() {
+    //
     var nomeUtente = document.getElementById("myInput").value;
     var distanzaKm = document.getElementById("myInput2").value;
     var sesso = document.getElementById("sesso").value;
-    console.log(nomeUtente, distanzaKm, sesso);
 
     var prezzoBiglietto = distanzaKm * 0.21;
-    console.log(prezzoBiglietto);
 
     var prezzoScontato;
 
+    var numeroCarozza = Math.ceil(Math.random() * 10);
+    var codiceCP = Math.floor(Math.random() * (100000 - 90000 + 1) + 90000)
+
     if (sesso === "minorenne") {
         prezzoScontato = prezzoBiglietto - ((prezzoBiglietto / 10) * 2);
+        document.getElementById("offerta").innerHTML = "Sconto Minori";
+
     } else if (sesso === "over65") {
         prezzoScontato = prezzoBiglietto - ((prezzoBiglietto / 10) * 4);
+        document.getElementById("offerta").innerHTML = "Sconto Over 65";
+
     } else if (sesso === "maggiorenne") {
         prezzoScontato = prezzoBiglietto;
     }
-    console.log(prezzoScontato);
+
+
+    document.getElementById("nomepasseggero").innerHTML = nomeUtente;
+    document.getElementById("carozza").innerHTML = numeroCarozza;
+    document.getElementById("codiceCP").innerHTML = codiceCP;
+    document.getElementById("costoBiglietto").innerHTML = prezzoScontato.toFixed(2);
 
 
 
+
+
+}
+
+
+function s2() {
+    var nomeUtente = document.getElementById("myInput").value = "";
+    var distanzaKm = document.getElementById("myInput2").value = "";
 
 }
